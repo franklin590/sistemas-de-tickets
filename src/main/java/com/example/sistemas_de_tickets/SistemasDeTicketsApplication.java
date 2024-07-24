f1ab49ebfdf933bd232ec498da012e54697c6fcd
Astral <franklin.melgar.rodriguez@gmail.com>
2024-07-24 09:26:04 -0600

    se agrega el proyecto

---
+++ src/main/java/com/example/sistemas_de_tickets/SistemasDeTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistemas_de_tickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemasDeTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemasDeTicketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sistemas_de_tickets/SistemasDeTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistemas_de_tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemasDeTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
